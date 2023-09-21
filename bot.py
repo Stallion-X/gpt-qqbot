a1782d9a1b711d7c7418221880c3048923c238fc
Stallion-X <993761206@qq.com>
2023-09-21 23:58:28 +0800

    first commit

--- bot.py
+++ bot.py
@@ -1,3 +1,5 @@
+import re
+import openai
 from asyncer import asyncify
 from creart import create
 from graia.ariadne.app import Ariadne
@@ -20,11 +22,15 @@ app = Ariadne(config(qq, "", HttpClientConfig("http://localhost:16080"),
                      WebsocketClientConfig("ws://localhost:16080")))
 friendDict = {}
 groupDict = {}
+admin_mode = False
 
 
 # TODO 请求失败时的异常处理，不然协程卡住，一直add waiting queue而不会再次请求
 # TODO 说明书
+# TODO 显示已注册的群组和私聊列表
 # DONE 查询余额
+# DONE 管理员模式 手动热切换key
+# TODO 管理员模式查看当前所有sessions 注册或反注册
 # TODO 插件注册模式对接不同api后端
 # TODO 重构架构（指令处理类等）
 # TODO saya
@@ -35,6 +41,10 @@ groupDict = {}
 @app.broadcast.receiver("FriendMessage")
 async def friend_chat(app: Ariadne, friend: Friend, message: MessageChain):
     if friend.id != qq:
+        if message.display == '#admin':
+            return
+        if friend.id == admin and admin_mode is True:
+            return
         if friend.id not in friendDict:
             friendDict[friend.id] = ChatGPT()
             logger.success(f"friend {friend.id} registered")
@@ -44,6 +54,7 @@ async def friend_chat(app: Ariadne, friend: Friend, message: MessageChain):
                 MessageChain(f"clear memory..."),
             )
         if message.display == '#balance':
+            logger.success('balance querying...')
             try:
                 await app.send_message(
                     friend,
@@ -54,6 +65,14 @@ async def friend_chat(app: Ariadne, friend: Friend, message: MessageChain):
             except Exception as e:
                 logger.error(e)
                 return
+        elif message.display == "#reload":
+            await asyncify(friendDict[friend.id].save)()
+            logger.success("save friend memory complete")
+            del friendDict[friend.id]
+            friendDict.pop(friend.id, None)
+            friendDict[friend.id] = ChatGPT()
+            logger.success(f"friend {friend.id} reloaded")
+            return
         if friendDict[friend.id].inuse is False and not friendDict[friend.id].waitQueue:  # session未在被使用，等待队列为空
             await app.send_message(friend, MessageChain(f"{await friendDict[friend.id].push(message.display)}"))
         if friendDict[friend.id].inuse is True:  # session正在被使用
@@ -62,7 +81,7 @@ async def friend_chat(app: Ariadne, friend: Friend, message: MessageChain):
             if len(friendDict[friend.id].waitQueue) > 3:
                 await app.send_message(
                     friend,
-                    MessageChain(f"正在等待前一条回复，少安毋躁..."),
+                    MessageChain(f"正在等待前一条回复，少安毋躁...（bot异步协程升级中，bug较多，如遇一直卡等待请使用\"#reload\"）"),
                 )
         if friendDict[friend.id].inuse is False and friendDict[friend.id].waitQueue:  # session未在被使用，等待队列不为空
             while friendDict[friend.id].waitQueue:
@@ -80,6 +99,7 @@ async def group_chat(app: Ariadne, group: Group, member: Member, message: Messag
                 MessageChain(f"clear memory..."),
             )
         if message.display == '#balance':
+            logger.success('balance querying...')
             try:
                 await app.send_message(
                     group,
@@ -126,9 +146,27 @@ async def group_chat(app: Ariadne, group: Group, member: Member, message: Messag
 
 @app.broadcast.receiver("FriendMessage")
 async def command_line(app: Ariadne, friend: Friend, message: MessageChain):
+    global admin_mode
     if friend.id == admin:
-        pass
-        # await app.send_message(friend, MessageChain([Plain("Hello, World!")]))
+        if message.display == '#admin' and admin_mode is False:
+            admin_mode = True
+            logger.success('admin access granted')
+            return
+        if message.display == '#exit' and admin_mode is True:
+            admin_mode = False
+            logger.success('admin exited')
+            return
+        if admin_mode is True:
+            if message.display == '#key':
+                await app.send_message(
+                    friend,
+                    MessageChain(f"{openai.api_key}"),
+                )
+                logger.info(f'get key {openai.api_key}')
+            if re.match("#changekey:", message.display):
+                openai.api_key = message.display.split(':')[1]
+                logger.info(f"switched to key {openai.api_key}")
+            # await app.send_message(friend, MessageChain([Plain("Hello, World!")]))
 
 
 Ariadne.launch_blocking()
